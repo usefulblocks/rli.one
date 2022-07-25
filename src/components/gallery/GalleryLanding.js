30ab6d90decc0d29634b3a133dab82aa3678973b
usefulblocks <hojlund82@gmail.com>
2022-07-25 11:15:46 +0200

    added node_modules

---
+++ src/components/gallery/GalleryLanding.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+const GalleryLanding = () => {
+    return(
+      <h2>Gallery starts here</h2>
+    );
+  }
+
+export default GalleryLanding
---
+++ src/views/Gallery.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// import sections
+import Gallery from '../components/gallery/GalleryLanding';
+
+
+const Login = () => {
+
+  return (
+    <>
+      <Gallery/>
+    </>
+  );
+}
+
+export default Login;
